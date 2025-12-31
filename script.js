decd86434a590f9841dd933a0b09d4048126efac
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-31 23:18:36 +0500

    Initial commit

--- script.js
+++ script.js
@@ -8,16 +8,16 @@
 //     this.price = price;
 // }
 
-let productData = [
-    new Product("1", "Durvesh", "Grapefruit, Lemon, Bergamot", "https://saeedghani.pk/cdn/shop/files/durvesh_1024x1024.jpg?v=1756815246", "4,990"),
-    new Product("2", "Mir", "Lavender, Birch", "https://saeedghani.pk/cdn/shop/files/MIR-100ml_1024x1024.webp?v=1759673223", "4,490"),
-    new Product("3", "Khan", "Lemon, Mint, Pink Pepper, Grapefruit", "https://saeedghani.pk/cdn/shop/files/KHAN100ml_1024x1024.webp?v=1759673121", "4,490"),
-    new Product("4", "Murshid", "Bergamot, Blackcurrant, Pineapple", "https://saeedghani.pk/cdn/shop/files/murshid12ml_1024x1024.webp?v=1759668874", "1,890"),
-    new Product("5", "Jalal", "Orange, Mandarin Orange, Bergamot, Orange Blossom", "https://saeedghani.pk/cdn/shop/files/Fate_1024x1024.webp?v=1759664914", "1,690"),
-    new Product("6", "Sultan", "Cardamom and Amber", "https://saeedghani.pk/cdn/shop/files/Sultan_1024x1024.webp?v=1759673367", "3,990"),
-    new Product("7", "Desire", "Apple, Lemon, Neroli And Bergamot", "https://saeedghani.pk/cdn/shop/files/Desire_1024x1024.jpg?v=1758541178", "3,990"),
-    new Product("8", "Haider Sports", "Fresh citrus, aquatic", "https://saeedghani.pk/cdn/shop/files/HS_1024x1024.webp?v=1764273648", "4,990")
-];
+// let productData = [
+//     new Product("1", "Durvesh", "Grapefruit, Lemon, Bergamot", "https://saeedghani.pk/cdn/shop/files/durvesh_1024x1024.jpg?v=1756815246", "4,990"),
+//     new Product("2", "Mir", "Lavender, Birch", "https://saeedghani.pk/cdn/shop/files/MIR-100ml_1024x1024.webp?v=1759673223", "4,490"),
+//     new Product("3", "Khan", "Lemon, Mint, Pink Pepper, Grapefruit", "https://saeedghani.pk/cdn/shop/files/KHAN100ml_1024x1024.webp?v=1759673121", "4,490"),
+//     new Product("4", "Murshid", "Bergamot, Blackcurrant, Pineapple", "https://saeedghani.pk/cdn/shop/files/murshid12ml_1024x1024.webp?v=1759668874", "1,890"),
+//     new Product("5", "Jalal", "Orange, Mandarin Orange, Bergamot, Orange Blossom", "https://saeedghani.pk/cdn/shop/files/Fate_1024x1024.webp?v=1759664914", "1,690"),
+//     new Product("6", "Sultan", "Cardamom and Amber", "https://saeedghani.pk/cdn/shop/files/Sultan_1024x1024.webp?v=1759673367", "3,990"),
+//     new Product("7", "Desire", "Apple, Lemon, Neroli And Bergamot", "https://saeedghani.pk/cdn/shop/files/Desire_1024x1024.jpg?v=1758541178", "3,990"),
+//     new Product("8", "Haider Sports", "Fresh citrus, aquatic", "https://saeedghani.pk/cdn/shop/files/HS_1024x1024.webp?v=1764273648", "4,990")
+// ];
 
 function showProducts() {
     for (let i = 0; i < productData.length; i++) {
