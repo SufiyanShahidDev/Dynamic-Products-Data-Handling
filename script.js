4343d9bb0658b534419d3a2a5a0509c06849d201
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-08 12:35:41 +0500

    Initial commit

--- script.js
+++ script.js
@@ -42,3 +42,14 @@ function showProducts() {
 }
 
 showProducts();
+
+
+function toggleBtn() {
+  var toggleBtn = document.getElementById("input");
+
+  if (toggleBtn.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+}
